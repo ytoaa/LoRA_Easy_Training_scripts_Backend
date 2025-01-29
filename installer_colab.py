aabfd4d2d3a3ec3b5817d0f0bce9be81e5d4d044
ytoaa <naworldw@gmail.com>
2025-01-30 02:22:29 +0900

    onnxruntime-gpu version change

--- installer_colab.py
+++ installer_colab.py
@@ -80,7 +80,7 @@ def setup_accelerate(platform: str) -> None:
 
 def setup_venv(venv_pip):
     subprocess.check_call(
-        f"{venv_pip} install -U torch==2.5.0 torchvision==0.20.0 'numpy<2' onnxruntime-gpu==1.19.2 --index-url https://download.pytorch.org/whl/cu124",
+        f"{venv_pip} install -U torch==2.5.0 torchvision==0.20.0 'numpy<2' onnxruntime-gpu --index-url https://download.pytorch.org/whl/cu124",
         shell=PLATFORM == "linux",
     )
     if PLATFORM == "windows":
